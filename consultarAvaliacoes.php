06463671e1292c1fe9ee699e3551e853fc1fe0ef
Marcelo Júnior <rokermarcelo@gmail.com>
2017-07-16 10:09:48 -0300

    Adicionadas as páginas do sistema a div#carregaPagina auxiliar Ela é responsável por "mudar" o conteúdo da página a ser carregada O arquivo menu.js foi alterado para atender a mudança supracitada

--- consultarAvaliacoes.php
+++ consultarAvaliacoes.php
@@ -14,104 +14,107 @@
         <?php include './includes/cabecalho.php'; ?>
         <?php include './includes/menu.php'; ?>
         
-        <section id="conteudo">
-            <?php include './includes/popup.php'; ?>
-            <h2>Consultar Avaliações</h2>
-            <form action="phpFuncoes/consultarAvaliacoes.php" method="post">
-                <label for="sltEvento">Evento:</label>
-                <select id="sltEvento" name="evento" autofocus>
-                    <option value="0">Selecione</option>
-                    <?php
-                        $eventos = array(1, "Expotec", 2, "Inexistente");
+        <div id="carregaPagina">
+            <section id="conteudo">
+                <?php include './includes/popup.php'; ?>
+                <h2>Consultar Avaliações</h2>
+                <form action="phpFuncoes/consultarAvaliacoes.php" method="post">
+                    <label for="sltEvento">Evento:</label>
+                    <select id="sltEvento" name="evento" autofocus>
+                        <option value="0">Selecione</option>
+                        <?php
+                            $eventos = array(1, "Expotec", 2, "Inexistente");
 
-                        for($i = 0; $i < count($eventos); $i = $i+2){
-                            if($idEvento == $eventos[$i]){
-                                echo "<option value='".$eventos[$i]."' selected>".$eventos[$i+1]."</option>";
+                            for($i = 0; $i < count($eventos); $i = $i+2){
+                                if($idEvento == $eventos[$i]){
+                                    echo "<option value='".$eventos[$i]."' selected>".$eventos[$i+1]."</option>";
+                                }
+                                else{
+                                    echo "<option value='".$eventos[$i]."'>".$eventos[$i+1]."</option>";
+                                }
                             }
-                            else{
-                                echo "<option value='".$eventos[$i]."'>".$eventos[$i+1]."</option>";
-                            }
-                        }
-                    ?>
-                </select>
-                
-                <label for="sltAvaliador">Avaliador:</label>
-                <select id="sltAvaliador" name="avaliador">
-                    <option value="0">Selecione</option>
-                    <?php
-                        $avaliadores = array(3, "Marcelo Júnior", 7, "Larissa Richelly");
+                        ?>
+                    </select>
 
-                        for($i = 0; $i < count($avaliadores); $i = $i+2){
-                            if($idUsuario == $avaliadores[$i]){
-                                echo "<option value='".$avaliadores[$i]."' selected>".$avaliadores[$i+1]."</option>";
-                            }
-                            else{
-                                echo "<option value='".$avaliadores[$i]."'>".$avaliadores[$i+1]."</option>";
-                            }
-                        }
-                    ?>
-                </select>
+                    <label for="sltAvaliador">Avaliador:</label>
+                    <select id="sltAvaliador" name="avaliador">
+                        <option value="0">Selecione</option>
+                        <?php
+                            $avaliadores = array(3, "Marcelo Júnior", 7, "Larissa Richelly");
 
-                <label for="sltStatusTrabalho">Status do trabalho:</label>
-                <select id="sltStatusTrabalho" name="statusTrabalho">
-                    <option value="0">Selecione</option>
-                    <?php
-                        $statusTrabalho = array(1, "Enviado", 2, "Em avaliação", 3, "Status de teste");
+                            for($i = 0; $i < count($avaliadores); $i = $i+2){
+                                if($idUsuario == $avaliadores[$i]){
+                                    echo "<option value='".$avaliadores[$i]."' selected>".$avaliadores[$i+1]."</option>";
+                                }
+                                else{
+                                    echo "<option value='".$avaliadores[$i]."'>".$avaliadores[$i+1]."</option>";
+                                }
+                            }
+                        ?>
+                    </select>
 
-                        for($i = 0; $i < count($statusTrabalho); $i = $i+2){
-                            if($idStatusTrabalho == $statusTrabalho[$i]){
-                                echo "<option value='".$statusTrabalho[$i]."' selected>".$statusTrabalho[$i+1]."</option>";
-                            }
-                            else{
-                                echo "<option value='".$statusTrabalho[$i]."'>".$statusTrabalho[$i+1]."</option>";
-                            }
-                        }
-                    ?>
-                </select>
-                
-                <label for="sltConcluida">Avaliações:</label>
-                <select id="sltConcluida" name="concluida">
-                    <option value="2">Todas</option>
-                    <?php
-                        $concluidas = array(0, "Em andamento", 1, "Concluídas");
+                    <label for="sltStatusTrabalho">Status do trabalho:</label>
+                    <select id="sltStatusTrabalho" name="statusTrabalho">
+                        <option value="0">Selecione</option>
+                        <?php
+                            $statusTrabalho = array(1, "Enviado", 2, "Em avaliação", 3, "Status de teste");
 
-                        for($i = 0; $i < count($concluidas); $i = $i+2){
-                            if($concluida == $concluidas[$i]){
-                                echo "<option value='".$concluidas[$i]."' selected>".$concluidas[$i+1]."</option>";
+                            for($i = 0; $i < count($statusTrabalho); $i = $i+2){
+                                if($idStatusTrabalho == $statusTrabalho[$i]){
+                                    echo "<option value='".$statusTrabalho[$i]."' selected>".$statusTrabalho[$i+1]."</option>";
+                                }
+                                else{
+                                    echo "<option value='".$statusTrabalho[$i]."'>".$statusTrabalho[$i+1]."</option>";
+                                }
                             }
-                            else{
-                                echo "<option value='".$concluidas[$i]."'>".$concluidas[$i+1]."</option>";
-                            }
-                        }
-                    ?>
-                </select>
-                
-                <label for="sltEscopo">Escopo:</label>
-                <select id="sltEscopo" name="escopo">
-                    <option value="2">Todos</option>
-                    <?php
-                        $escopos = array(0, "Submetidos", 1, "Aprovados");
+                        ?>
+                    </select>
 
-                        for($i = 0; $i < count($escopos); $i = $i+2){
-                            if($ehFinal == $escopos[$i]){
-                                echo "<option value='".$escopos[$i]."' selected>".$escopos[$i+1]."</option>";
+                    <label for="sltConcluida">Avaliações:</label>
+                    <select id="sltConcluida" name="concluida">
+                        <option value="2">Todas</option>
+                        <?php
+                            $concluidas = array(0, "Em andamento", 1, "Concluídas");
+
+                            for($i = 0; $i < count($concluidas); $i = $i+2){
+                                if($concluida == $concluidas[$i]){
+                                    echo "<option value='".$concluidas[$i]."' selected>".$concluidas[$i+1]."</option>";
+                                }
+                                else{
+                                    echo "<option value='".$concluidas[$i]."'>".$concluidas[$i+1]."</option>";
+                                }
                             }
-                            else{
-                                echo "<option value='".$escopos[$i]."'>".$escopos[$i+1]."</option>";
+                        ?>
+                    </select>
+
+                    <label for="sltEscopo">Escopo:</label>
+                    <select id="sltEscopo" name="escopo">
+                        <option value="2">Todos</option>
+                        <?php
+                            $escopos = array(0, "Submetidos", 1, "Aprovados");
+
+                            for($i = 0; $i < count($escopos); $i = $i+2){
+                                if($ehFinal == $escopos[$i]){
+                                    echo "<option value='".$escopos[$i]."' selected>".$escopos[$i+1]."</option>";
+                                }
+                                else{
+                                    echo "<option value='".$escopos[$i]."'>".$escopos[$i+1]."</option>";
+                                }
                             }
-                        }
-                    ?>
-                </select>
-                
-                <label for="txtTitulo">Trecho do título:</label>
-                <input type="text" id="txtTitulo" name="titulo">
-                <label for="txtResumo">Trecho do resumo:</label>
-                <input type="text" id="txtResumo" name="resumo">
-                <input type="submit" value="Consultar">
-            </form>
-            <div id="atualizavel">
-            </div>
-        </section>
+                        ?>
+                    </select>
+
+                    <label for="txtTitulo">Trecho do título:</label>
+                    <input type="text" id="txtTitulo" name="titulo">
+                    <label for="txtResumo">Trecho do resumo:</label>
+                    <input type="text" id="txtResumo" name="resumo">
+                    <input type="submit" value="Consultar">
+                </form>
+                <div id="atualizavel">
+                </div>
+            </section>
+        </div>
+        
         <?php include './includes/rodape.php'; ?>
     </body>
 </html>
--- crudStatusTrabalho.php
+++ crudStatusTrabalho.php
@@ -18,58 +18,60 @@
         <?php include './includes/cabecalho.php'; ?>
         <?php include './includes/menu.php'; ?>
         
-        <section id="conteudo">
-            <!-- O CONTEÚDO DAS PÁGINAS DEVE APARECER AQUI -->
-            <?php include './includes/popup.php'; ?>
-            <h2>CRUDs para Status Trabalho</h2>
-            <p>Lista todos Status já cadastrados</p>
-            <div id="atualizavel">
-                <?php
-                    $resultado = StatusTrabalho::getTodosStatusTrabalho();
-                    if($resultado != null){
-                        echo "<ul>";
-                        while($obj = $resultado->fetch_object()){
-                            echo "<li>Id: ".$obj->idStatusTrabalho." - Descrição: ".$obj->descricao."</li>";
-                        }
-                        echo "</ul>";
-                    }
-                ?>
-            </div>
-            <p>Formulário para criar novo Status Trabalho</p>
-            <form action="phpFuncoes/cadastrarStatusTrabalho.php" method="post">
-                <label for="txtNomeStatusTrabalho">Nome do Status a ser criado: </label>
-                <input type="text" id="txtNomeStatusTrabalho" name="pStatusTrabalho" placeholder="Novo Status">
-                <input type="submit" value="Salvar">
-            </form>
-            <p>Formulário para editar Status Trabalho</p>
-            <form action="phpFuncoes/editarStatusTrabalho.php" method="post">
-                <label for="sltStatusTrabalho">Selecione o Status a ser modificado: </label>
-                <select id="sltStatusTrabalho" name="pIdStatusTrabalho">
+        <div id="carregaPagina">
+            <section id="conteudo">
+                <!-- O CONTEÚDO DAS PÁGINAS DEVE APARECER AQUI -->
+                <?php include './includes/popup.php'; ?>
+                <h2>CRUDs para Status Trabalho</h2>
+                <p>Lista todos Status já cadastrados</p>
+                <div id="atualizavel">
                     <?php
                         $resultado = StatusTrabalho::getTodosStatusTrabalho();
-                        while($obj = $resultado->fetch_object()){
-                            echo "<option value='".$obj->idStatusTrabalho."'>".$obj->descricao."</option>";
+                        if($resultado != null){
+                            echo "<ul>";
+                            while($obj = $resultado->fetch_object()){
+                                echo "<li>Id: ".$obj->idStatusTrabalho." - Descrição: ".$obj->descricao."</li>";
+                            }
+                            echo "</ul>";
                         }
                     ?>
-                </select>
-                <label for="txtNovoNomeStatusTrabalho">Informe a nova descrição do Status: </label>
-                <input type="text" id="txtNovoNomeStatusTrabalho" name="pStatusTrabalho" placeholder="Nova descrição">
-                <input type="submit" value="Salvar">
-            </form>
-            <p>Formulário para excluir Status Trabalho</p>
-            <form action="phpFuncoes/excluirStatusTrabalho.php" method="post">
-                <label for="sltStatusTrabalho">Selecione o Status a ser excluído: </label>
-                <select id="sltStatusTrabalho" name="pIdStatusTrabalho">
-                    <?php
-                        $resultado = StatusTrabalho::getTodosStatusTrabalho();
-                        while($obj = $resultado->fetch_object()){
-                            echo "<option value='".$obj->idStatusTrabalho."'>".$obj->descricao."</option>";
-                        }
-                    ?>
-                </select>
-                <input type="submit" value="Excluir">
-            </form>
-        </section>
+                </div>
+                <p>Formulário para criar novo Status Trabalho</p>
+                <form action="phpFuncoes/cadastrarStatusTrabalho.php" method="post">
+                    <label for="txtNomeStatusTrabalho">Nome do Status a ser criado: </label>
+                    <input type="text" id="txtNomeStatusTrabalho" name="pStatusTrabalho" placeholder="Novo Status">
+                    <input type="submit" value="Salvar">
+                </form>
+                <p>Formulário para editar Status Trabalho</p>
+                <form action="phpFuncoes/editarStatusTrabalho.php" method="post">
+                    <label for="sltStatusTrabalho">Selecione o Status a ser modificado: </label>
+                    <select id="sltStatusTrabalho" name="pIdStatusTrabalho">
+                        <?php
+                            $resultado = StatusTrabalho::getTodosStatusTrabalho();
+                            while($obj = $resultado->fetch_object()){
+                                echo "<option value='".$obj->idStatusTrabalho."'>".$obj->descricao."</option>";
+                            }
+                        ?>
+                    </select>
+                    <label for="txtNovoNomeStatusTrabalho">Informe a nova descrição do Status: </label>
+                    <input type="text" id="txtNovoNomeStatusTrabalho" name="pStatusTrabalho" placeholder="Nova descrição">
+                    <input type="submit" value="Salvar">
+                </form>
+                <p>Formulário para excluir Status Trabalho</p>
+                <form action="phpFuncoes/excluirStatusTrabalho.php" method="post">
+                    <label for="sltStatusTrabalho">Selecione o Status a ser excluído: </label>
+                    <select id="sltStatusTrabalho" name="pIdStatusTrabalho">
+                        <?php
+                            $resultado = StatusTrabalho::getTodosStatusTrabalho();
+                            while($obj = $resultado->fetch_object()){
+                                echo "<option value='".$obj->idStatusTrabalho."'>".$obj->descricao."</option>";
+                            }
+                        ?>
+                    </select>
+                    <input type="submit" value="Excluir">
+                </form>
+            </section>
+        </div>
         <?php include './includes/rodape.php'; ?>
     </body>
 </html>
